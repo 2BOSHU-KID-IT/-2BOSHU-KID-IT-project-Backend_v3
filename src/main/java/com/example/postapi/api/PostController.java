bdd4eecb2de14ddf2bb645382cfbfe25677d4cf8
이정윤 <dlwjddbs14@naver.com>
2021-12-02 02:53:33 +0900

    ⚡ :: 검색기능 컨트롤러 추가

--- src/main/java/com/example/postapi/api/PostController.java
+++ src/main/java/com/example/postapi/api/PostController.java
@@ -2,18 +2,18 @@ package com.example.postapi.api;
 
 import com.example.postapi.entity.Post;
 import com.example.postapi.request.PostRequest;
-import com.example.postapi.service.PostService;
+import com.example.postapi.service.PostServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/post")
 public class PostController {
 
-    private final PostService postService;
+    private final PostServiceImpl postService;
 
     @PostMapping("/create")
     public void create(@RequestBody PostRequest request){
@@ -21,16 +21,22 @@ public class PostController {
     }
 
     @GetMapping("/list")
-    public List<Post> getAllPost(){
-        return postService.getAllPost();
+    public List<Post> getAllPosts(){
+        return postService.getAllPosts();
     }
 
-    @PatchMapping("/{id}")
+    @GetMapping("/search")
+    public List<Post> search(@RequestParam(value = "keyword") String keyword, Pageable pageable) {
+        return postService.search(keyword, pageable);
+    }
+
+
+    @PatchMapping("/update/{id}")
     public void update(@PathVariable Integer id, @RequestBody PostRequest request){
         postService.update(id, request);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public void delete(@PathVariable Integer id){
         postService.delete(id);
     }
