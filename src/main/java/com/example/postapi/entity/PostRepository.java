de9464caec60f9030912d59838fa6511b5f552f6
이정윤 <dlwjddbs14@naver.com>
2021-12-02 02:44:03 +0900

    ⚡ :: findByNameContaining 추가

--- src/main/java/com/example/postapi/entity/PostRepository.java
+++ src/main/java/com/example/postapi/entity/PostRepository.java
@@ -1,6 +1,10 @@
 package com.example.postapi.entity;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findByNameContaining(String keyword, Pageable pageable);
 }
