5fc6f7055d02faea964e37ba6cd669512e3dfa94
이정윤 <dlwjddbs14@naver.com>
2021-12-02 22:14:47 +0900

    ⚡ :: security.filter ➡ security

--- src/main/java/com/example/postapi/security/SecurityConfig.java
+++ src/main/java/com/example/postapi/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.postapi.security.filter;
+package com.example.postapi.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
