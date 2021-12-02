0a9df6a085b82750fe892e949e09e49dac51f0b3
이정윤 <dlwjddbs14@naver.com>
2021-12-02 13:09:32 +0900

    ⚡ :: Query문으로 하트 수로 내림차순 정렬

--- src/main/java/com/example/postapi/entity/PostRepository.java
+++ src/main/java/com/example/postapi/entity/PostRepository.java
@@ -2,9 +2,13 @@ package com.example.postapi.entity;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findByNameContaining(String keyword, Pageable pageable);
+
+    @Query(value = "select * from post order by heart desc", nativeQuery = true)
+    List<Post> desHeart();
 }
