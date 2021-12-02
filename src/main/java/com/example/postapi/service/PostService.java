67991bb35b0390476e7a2ca6eb348d401ad4d5d9
이정윤 <dlwjddbs14@naver.com>
2021-12-02 13:08:54 +0900

    ⚡ :: Post 서비스 메서드 수정

--- src/main/java/com/example/postapi/service/PostService.java
+++ src/main/java/com/example/postapi/service/PostService.java
@@ -1,15 +1,12 @@
 package com.example.postapi.service;
 
 import com.example.postapi.entity.Post;
-import com.example.postapi.request.PostRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface PostService {
-    void create(PostRequest request);
     List<Post> getAllPosts();
-    List<Post> search(String keyword, Pageable pageable);
-    void update(Integer id, PostRequest request);
-    void delete(Integer id);
+    List<Post> searchPosts(String keyword, Pageable pageable);
+    List<Post> desHeart();
 }
--- src/main/java/com/example/postapi/service/PostServiceImpl.java
+++ src/main/java/com/example/postapi/service/PostServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.postapi.service;
 
 import com.example.postapi.entity.Post;
 import com.example.postapi.entity.PostRepository;
-import com.example.postapi.request.PostRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -11,41 +10,22 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class PostServiceImpl implements PostService{
+public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
 
     @Override
-    public void create(PostRequest request){
-        Post post = Post.builder()
-                .address(request.getAddress())
-                .division(request.getDivision())
-                .name(request.getName())
-                .number(request.getNumber())
-                .heart(request.getHeart())
-                .build();
-
-        postRepository.save(post);
-    }
-
-    @Override
-    public List<Post> getAllPosts(){
+    public List<Post> getAllPosts() {
         return postRepository.findAll();
     }
 
     @Override
-    public List<Post> search(String keyword, Pageable pageable){
+    public List<Post> searchPosts(String keyword, Pageable pageable) {
         return postRepository.findByNameContaining(keyword, pageable);
     }
 
     @Override
-    public void update(Integer id, PostRequest request){
-        Post post = postRepository.findById(id).get();
-        post.setHeart(request.getHeart());
-    }
-
-    @Override
-    public void delete(Integer id){
-        postRepository.deleteById(id);
+    public List<Post> desHeart() {
+        return postRepository.desHeart();
     }
 }
